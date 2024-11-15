3c7f6d1b7b2b5dfa677dae805213f542559528de
UdlaAriel <ariel.anchapaxi@udla.edu.ec>
2024-11-15 08:51:41 -0500

    Grid Page and Stack Page were updated.

--- MauiAppEightWeek/Views/GridPage.xaml.cs
+++ MauiAppEightWeek/Views/GridPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class GridPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new StackPage());
+    }
 }
