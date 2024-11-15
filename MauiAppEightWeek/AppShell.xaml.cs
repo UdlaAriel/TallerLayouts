7f76f29700bbe0705c418a226f5eb15718676773
ARIEL <ARIEL@DESKTOP-BEFKEM8>
2024-11-15 07:35:10 -0500

    Agregar archivos de proyecto.

---
+++ MauiAppEightWeek/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppEightWeek
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiAppEightWeek/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppEightWeek
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
